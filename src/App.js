459d6494dddc282cb6cd041d4fa98d20acc79aa4
aletta-bookwitch <aleta333@gmail.com>
2022-03-30 19:24:40 +0200

    Link and bg color

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Example React App
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/aletta-bookwitch/example_react_app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub Repo
         </a>
       </header>
     </div>

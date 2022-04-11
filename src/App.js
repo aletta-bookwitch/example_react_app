401747aa29e1596254fbaba8af09cae391bba802
aletta-bookwitch <aleta333@gmail.com>
2022-04-11 20:17:51 +0200

    Added form component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { RandomValue } from "./components/RandomValue";
 import { RepositoryLink } from "./components/RepositoryLink";
 import { Quiz } from "./components/Quiz";
 import { useState } from "react";
+import { LoginForm } from "./components/LoginForm";
 
 function App() {
   const [actionCount, setActionCount] = useState(0);
@@ -14,6 +15,7 @@ function App() {
     setQuizAnswer({ answers, id });
   };
   const handleStateChange = () => setActionCount((state) => state + 1);
+  const handleLogin = (formState) => console.log(formState);
 
   return (
     <div className="App">
@@ -22,9 +24,10 @@ function App() {
         <h1>Example React App</h1>
         <p>Your unique ID is: {idHelpers.generateId()}</p>
         <RepositoryLink>View Repository</RepositoryLink>
+        <LoginForm onLogin={handleLogin} />
         <RandomValue values={[4, 5, 6]} />
         <p>Your action count is: {actionCount}</p>
-        
+
         {quizAnswer === null && (
           <Quiz id={id} onSubmit={handleSubmit} onStateChange={handleStateChange} />
         )}
